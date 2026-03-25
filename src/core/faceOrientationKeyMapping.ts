c56990ceb27a37c9cec0bdc679e556b61252239c
David Goncalves <dmg@secondexit.com>
2026-03-25 13:41:41 -0400

    fix: Correct rotation lookup for bottom and left faces

--- src/core/faceOrientationKeyMapping.ts
+++ src/core/faceOrientationKeyMapping.ts
@@ -43,9 +43,9 @@ export const KEY_MAP = {
     180: { w: [0, -1, 0], x: [0, 1, 0], d: [0, 0, 1], a: [0, 0, -1], q: [1, 0, 0], z: [-1, 0, 0] },
     270: { x: [0, 0, 1], w: [0, 0, -1], d: [0, -1, 0], a: [0, 1, 0], q: [1, 0, 0], z: [-1, 0, 0] }
   }
-} as const;
+};
 
-// Rotation axes for brush/cube rotation keys (o, k, ., ;, i, p) per face & camera rotation
+// 2. ROTATION LOOKUP (o, k, ., ;)
 export const rotationLookup = {
   "front": {
     0: { o: [1, 0, 0], period: [-1, 0, 0], k: [0, 1, 0], semicolon: [0, -1, 0], i: [0, 0, 1], p: [0, 0, -1] },
@@ -66,10 +66,10 @@ export const rotationLookup = {
     270: { o: [1, 0, 0], period: [-1, 0, 0], k: [0, 0, -1], semicolon: [0, 0, 1], i: [0, 1, 0], p: [0, -1, 0] }
   },
   "bottom": {
-    0: { o: [0, 0, 1], period: [0, 0, -1], k: [-1, 0, 0], semicolon: [1, 0, 0], i: [0, 1, 0], p: [0, -1, 0] },
-    90: { o: [-1, 0, 0], period: [1, 0, 0], k: [0, 0, -1], semicolon: [0, 0, 1], i: [0, 1, 0], p: [0, -1, 0] },
-    180: { o: [0, 0, -1], period: [0, 0, 1], k: [1, 0, 0], semicolon: [-1, 0, 0], i: [0, 1, 0], p: [0, -1, 0] },
-    270: { o: [1, 0, 0], period: [-1, 0, 0], k: [0, 0, 1], semicolon: [0, 0, -1], i: [0, 1, 0], p: [0, -1, 0] }
+    0: { o: [0, 0, 1], period: [0, 0, -1], k: [-1, 0, 0], semicolon: [1, 0, 0] },
+    90: { o: [-1, 0, 0], period: [1, 0, 0], k: [0, 0, -1], semicolon: [0, 0, 1] },
+    180: { o: [0, 0, -1], period: [0, 0, 1], k: [1, 0, 0], semicolon: [-1, 0, 0] },
+    270: { o: [1, 0, 0], period: [-1, 0, 0], k: [0, 0, 1], semicolon: [0, 0, -1] }
   },
   "right": {
     0: { o: [0, 0, 1], period: [0, 0, -1], k: [0, -1, 0], semicolon: [0, 1, 0], i: [1, 0, 0], p: [-1, 0, 0] },
@@ -78,12 +78,12 @@ export const rotationLookup = {
     270: { o: [0, 0, -1], period: [0, 0, 1], k: [0, 1, 0], semicolon: [0, -1, 0], i: [1, 0, 0], p: [-1, 0, 0] }
   },
   "left": {
-    0: { o: [1, 0, 0], period: [-1, 0, 0], k: [0, 0, 1], semicolon: [0, 0, -1], i: [-1, 0, 0], p: [1, 0, 0] },
+    0: { o: [0, 1, 0], period: [0, -1, 0], k: [0, 0, 1], semicolon: [0, 0, -1], i: [-1, 0, 0], p: [1, 0, 0] },
     90: { o: [0, 0, -1], period: [0, 0, 1], k: [0, -1, 0], semicolon: [0, 1, 0], i: [-1, 0, 0], p: [1, 0, 0] },
     180: { o: [0, -1, 0], period: [0, 1, 0], k: [0, 0, -1], semicolon: [0, 0, 1], i: [-1, 0, 0], p: [1, 0, 0] },
     270: { o: [0, 0, 1], period: [-1, 0, 0], k: [0, 1, 0], semicolon: [0, -1, 0], i: [-1, 0, 0], p: [1, 0, 0] }
   }
-} as const;
+};
 
 export type CameraFace = keyof typeof KEY_MAP;
 export type CameraRotation = 0 | 90 | 180 | 270;
